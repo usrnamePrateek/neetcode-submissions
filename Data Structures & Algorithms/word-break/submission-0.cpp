14dfe4721013d78f0c3a9bc4d85827ae32e6072a
Prateek Purohit <151670884+usrnamePrateek@users.noreply.github.com>
2026-06-22 21:58:19 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+   public:
+   vector<int> dp;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        dp.resize(s.size(), -1);
+        unordered_set<string> wordDictSet;
+        for (string& word : wordDict) {
+            wordDictSet.insert(word);
+        }
+        return sol(s, 0, wordDictSet);
+    }
+
+    bool sol(string& s, int partIdx, unordered_set<string>& wordDict) {
+        if (partIdx == s.size()) {
+            return true;
+        }
+
+        if (dp[partIdx] != -1) {
+            return dp[partIdx] == 1;
+        }
+
+        string str = "";
+        bool res = false;
+        for (int i = partIdx; i < s.size(); i++) {
+            str.push_back(s[i]);
+            if (wordDict.count(str)) {
+                // cout << str << endl;
+                res |= sol(s, i + 1, wordDict);
+            }
+        }
+
+        dp[partIdx] = (res ? 1 : 0);
+        return res;
+    }
+};
